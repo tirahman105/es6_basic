d8e99510b1ab4c259dafe16cfa68bad39668a507
Tareq_Ibna_Rahman <tirahman105@gmail.com>
2023-02-22 19:31:48 +0600

    scope added

---
+++ scope.js
@@ -0,0 +1,12 @@
+// global scope
+// local scope
+// block scope ( started after es6 )
+
+// global scope variable 
+// যে কোনো জায়গা থেকে যে ভেরিয়েবল এর এক্সেস করা যায়। 
+
+// block scope variable
+// কোনো একটা ব্লক এর মধ্যে শুধু এক্সেস করা যায়। এর বাহিরে এক্সেস করা যায় না। 
+
+// যদি var দিয়ে কোনো ভেরিয়েবল ডিক্লেয়ার করা হয় সেটা global scope variable হয়। 
+// কিন্তু কোনো function এর ভেতরে কোনো variable নিলে সেটা ঐ function এর বাহিরে এক্সেস করা যায় না। var দিয়ে declare করলেও না। এটা local scope variable. 
